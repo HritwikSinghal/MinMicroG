6840610fd8194c83e6ae4db0509aa1754c176236
Hritwik <hritwiksinghal@protonmail.com>
2025-07-02 01:33:32 +0530

    shell: add aapt, apksigner

--- shell.nix
+++ shell.nix
@@ -1,9 +1,22 @@
 let
-  pkgs = import <nixpkgs> {};
-in pkgs.mkShell {
+  pkgs = import <nixpkgs> { };
+
+  aapt1 =
+    with pkgs;
+    aapt.overrideAttrs (oldAttrs: {
+      postInstall = ''
+        mv $out/bin/aapt2 $out/bin/aapt
+      '';
+    });
+
+in
+pkgs.mkShell {
   packages = with pkgs; [
     jq
     unzip
     zip
+    aapt1
+    apksigner
   ];
+
 }
